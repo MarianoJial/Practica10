280ec95bdf8e77caa4c6f9767c48ef1ed0d2363f
Mariano <kick_acosta@hotmail.com>
2017-03-14 20:29:01 -0600

    Add files via upload

---
+++ practica10/index.php
@@ -0,0 +1,16 @@
+<?php
+//Comentarios
+#Comentarios
+/*Comentarios
+multilinea*/
+echo "Hola mundo php";
+?>
+
+<html>
+    <head>
+    <title>Practica 10 php </title>
+    </head>
+<body>
+<h1>Hola mundo html</h1>    
+</body>
+</html>
---
+++ practica10/index2.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>practica 10 php</title>
+</head>
+<body>
+	<h1>usando PHP</h1>
+	</p><?php
+	echo = "Parrafo php";
+	?></p>
+	<p>Parrafo HTML</p>
+	<h3>
+	<?php
+	echo= "parrafo php";
+	?></h3>
+</body>
+</html>
